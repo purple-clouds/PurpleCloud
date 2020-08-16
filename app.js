43a96eff251da17c2dde508490adb084e15f21cd
theFl00f <celeste.ellerby@gmail.com>
2020-08-15 21:36:17 -0400

    setting up to push to heroku

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const Mongoose = require('mongoose');
+const BodyParser = require('body-parser')
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const numbersRouter = require('./routes/numbers');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,10 +19,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/numbers', numbersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -37,5 +41,9 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(3000, () => {
+  console.log('listening at :3000')
+})
 
 module.exports = app;
